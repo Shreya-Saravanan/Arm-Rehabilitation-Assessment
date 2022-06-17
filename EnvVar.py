86a3b4baf2e23b3fcde07e7e369a1fcdfeaa0e03
Hayagreev <50618967+nobodyh@users.noreply.github.com>
2022-06-17 13:48:18 +0530

    Update EnvVar.py

--- EnvVar.py
+++ EnvVar.py
@@ -12,15 +12,15 @@ def SetEnvVariables(email_option:int):
     """
     if email_option == 1:
         # Outlook Mail Details
-        os.environ['EMAIL_USER'] = 'email@outlook.com'
-        os.environ['EMAIL_PASSWORD'] = 'Outlook_Password'
+        EMAIL_ID = os.environ['OUTLOOK_EMAIL']
+        EMAIL_PASSWORD = os.environ['OUTLOOK_PASSWORD']
     
     elif email_option == 2:
         # GMail Details
-        os.environ['EMAIL_USER'] = 'email@gmail.com'
-        os.environ['EMAIL_PASSWORD'] = 'GMail_App_Password'
+        EMAIL_ID = os.environ['GMAIL_EMAIL']
+        EMAIL_PASSWORD = os.environ['GMAIL_PASSWORD']
         
     else:
         print('Invalid Email Option')
     
-    return (os.environ['EMAIL_USER'], os.environ['EMAIL_PASSWORD'])
+    return (EMAIL_ID, EMAIL_PASSWORD)
